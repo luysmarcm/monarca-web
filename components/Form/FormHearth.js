c7dfa9a1fc886231a52b4d7a8a06cd8733eb9902
luysmarcm <luysmar27@gmail.com>
2025-01-16 20:41:04 -0400

    Revertir cambios

--- components/Form/FormHearth.js
+++ components/Form/FormHearth.js
@@ -1,38 +1,31 @@
-'use client'
-import React,{useEffect} from 'react'
+import { useEffect } from "react";
+import Head from "next/head";
 
 const FormHearth = () => {
-    useEffect(() => {
-			const script = document.createElement("script");
-			script.src = "https://widget.gethearth.com/script.js";
-			script.id = "hearth-script";
-			script.setAttribute("data-orgid", "49438");
-			script.setAttribute("data-partner", "monarca-life-improvement");
-			script.setAttribute(
-				"data-orgUuid",
-				"9acc99cb-c1be-46fa-9ec8-2ea54a8e0b62"
-			);
-			script.async = true;
+	useEffect(() => {
+		const script = document.createElement("script");
+		script.src = "https://widget.gethearth.com/script.js";
+		script.id = "hearth-script";
+		script.async = true;
+		script.dataset.orgid = "49438";
+		script.dataset.partner = "monarca-life-improvement";
+		script.dataset.orgUuid = "9acc99cb-c1be-46fa-9ec8-2ea54a8e0b62";
+		document.body.appendChild(script);
 
-			// Listener para cuando el script se carga
-			script.onload = () => {
-				console.log("Script cargado correctamente");
-				// Aquí podrías inicializar el widget si es necesario
-			};
+		return () => {
+			document.body.removeChild(script);
+		};
+	}, []);
 
-			document.body.appendChild(script);
-
-			return () => {
-				document.body.removeChild(script);
-			};
-		}, []);
-  return (
-		<iframe
-			id="hearth-lead-capture-widget"
-			style={{ width: "100%", height: "500px", border: "1px solid black" }}
-			title="Hearth Lead Capture Widget"
-		></iframe>
+	return (
+		<>
+			<iframe
+				id="hearth-lead-capture-widget"
+				style={{ width: "100%", height: "500px", border: "1px solid black" }}
+				title="Hearth Lead Capture Widget"
+			></iframe>
+		</>
 	);
-}
+};
 
-export default FormHearth
+export default FormHearth;
