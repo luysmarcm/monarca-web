3c56285d882eae8b71b74025b443b1631fba5700
luysmarcm <luysmar27@gmail.com>
2025-02-06 23:13:11 -0400

    Modificacion del monto de ortorgamiento para proyecto, prueba google ads

--- app/[locale]/gtag.js
+++ app/[locale]/gtag.js
@@ -16,6 +16,17 @@ return (
         gtag('config', 'G-ZKDGGGZQGQ');
         `}
 		</Script>
+		<Script
+			src="https://www.googletagmanager.com/gtag/js?id=AW-11354093423"
+			strategy="afterInteractive"
+		/>
+		<Script id="google-analytics" strategy="afterInteractive">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-11354093423');
+        `}</Script>
 	</div>
 );
 }
