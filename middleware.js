ed3a02bbabf46c9293839baa07db3c024f095db2
Luysmar <luysmar92@gmail.com>
2026-04-15 12:52:24 -0400

    cambio del middleware

--- middleware.js
+++ middleware.js
@@ -1,17 +1,39 @@
 import createMiddleware from "next-intl/middleware";
-import { locales, localePrefix,pathnames } from "./navigation";
+import { locales, localePrefix, pathnames } from "./navigation";
 
-export default createMiddleware({
-	// A list of all locales that are supported
-	// locales: ["en", "es"],
-	localePrefix,
-  	locales,
-	pathnames
-	// Used when no locale matches
-	// defaultLocale: "en",
+const intlMiddleware = createMiddleware({
+  localePrefix,
+  locales,
+  pathnames
 });
 
+export default function middleware(req) {
+  const ua = req.headers.get('user-agent') || '';
+
+  const allowedBots = ['googlebot', 'bingbot'];
+  const isAllowed = allowedBots.some(b => ua.toLowerCase().includes(b));
+
+  const blockedBots = [
+    'bot',
+    'crawl',
+    'spider',
+    'curl',
+    'wget',
+    'python',
+    'axios'
+  ];
+
+  const isBlocked = blockedBots.some(b => ua.toLowerCase().includes(b));
+
+  // 🚫 Bloquear bots
+  if (isBlocked && !isAllowed) {
+    return new Response('Blocked', { status: 403 });
+  }
+
+  // ✅ continuar con next-intl
+  return intlMiddleware(req);
+}
+
 export const config = {
-	// Match only internationalized pathnames
-	matcher: ["/", "/(es|en)/:path*"],
+  matcher: ["/", "/(es|en)/:path*"],
 };
